adce5ceef90028c6b606944c3cc38d3cef927d69
Ángel Trujillo <37855041+Trujillo19@users.noreply.github.com>
2020-04-15 09:22:09 -0500

    Env variables update

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({
-    path: './config/development.env'
-});
+if (process.env.NODE_ENV !== 'production'){
+    dotenv.config({
+        path: './config/development.env'
+    });
+}
+
 
 process.on('uncaughtException', err => {
     console.log('Uncaught Exception. Shutting down...');
